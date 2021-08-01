985e9e7ea24a91c046f1105c5b9cba113cbb912f
R.J <rjschodowski@hotmail.com>
2021-08-01 14:05:35 -0400

    environment files updated and models.ts page created

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment as env } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  constructor(private HttpClient) { }
+  getGameList(
+    ordering: string,
+    search?: string
+  ): Observable<APIResponse<Game>> {
+    let params = new HttpParams().set('ordering', ordering);
+
+    if (search) {
+      params = new HttpParams().set('ordering', ordering).set('search', search);
+    }
+
+    return this.http.get<APIResponse<Game>>(`${env.BASE.URL}/games`, {
+      params: params,
+    });
+  }
 }
